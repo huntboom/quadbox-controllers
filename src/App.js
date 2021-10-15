32a231d5ee2a7f0654d88a775483af43ec6ee2f2
huntboom <hkarsseboom@hotmail.com>
2021-10-14 22:05:06 -0400

    resized the canvas in styles.css and imported it, fixed Orbit Controls by rendering HTML outside of the canvas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
 import {Canvas} from '@react-three/fiber'
+import './styles.css'
 import './Model.js'
 import Model from './Model.js';
 import { Suspense } from 'react'
@@ -8,7 +8,7 @@ import { Light } from 'three';
 
 const HTMLOutside = () => {
   return(
-    <div>Outside Canvas</div>
+    <h1>Outside Canvas</h1>
   )
 };
 
@@ -19,14 +19,14 @@ const HTMLInside = () => {
         <h1 className='title'>Inside Canvas</h1>
       </div>
     </Html>
-  )
+  );
 }
 const Scene = () => {
   return(
-    <Canvas colorManagement camera={{position: [0,0,100],fov:70}}>
-  <HTMLInside></HTMLInside>
+    <Canvas colorManagement camera={{position: [0,30,100],fov:70}}>
       <Suspense fallback={null}>
-      <OrbitControls/>
+     <pointLight intensity={1.12} position={[0, 50, 100]} />
+          <OrbitControls/>
    <Model/>
     
       </Suspense>
@@ -36,11 +36,14 @@ const Scene = () => {
   );
 }
 
-const App = () => {
-  return<Scene />
-      
+function App() {
+  return(
+    <>
+        <HTMLOutside/>  
+        <Scene />
+    </>
+  );
 };
 require('react-dom');
 window.React2 = require('react');
-console.log(window.React1 === window.React2);
 export default App;
